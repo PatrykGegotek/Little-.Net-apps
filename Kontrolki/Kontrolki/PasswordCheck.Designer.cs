9594a9b1aaf1547e69c55c7e9b22276954315e65
PatrykGegotek <patryk26g@gmail.com>
2022-05-24 00:03:18 +0200

    kontrolki

---
+++ Kontrolki/Kontrolki/PasswordCheck.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Kontrolki
+{
+    partial class PasswordCheck
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.charNum = new System.Windows.Forms.Label();
+            this.big = new System.Windows.Forms.Label();
+            this.special = new System.Windows.Forms.Label();
+            this.digit = new System.Windows.Forms.Label();
+            this.imChar = new System.Windows.Forms.PictureBox();
+            this.imBig = new System.Windows.Forms.PictureBox();
+            this.imSpecial = new System.Windows.Forms.PictureBox();
+            this.imDigit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imChar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imBig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imSpecial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imDigit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.title.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(0, 25);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(590, 42);
+            this.title.TabIndex = 0;
+            this.title.Text = "Password Requirements";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // charNum
+            // 
+            this.charNum.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.charNum.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.charNum.Location = new System.Drawing.Point(80, 107);
+            this.charNum.Name = "charNum";
+            this.charNum.Size = new System.Drawing.Size(510, 28);
+            this.charNum.TabIndex = 1;
+            this.charNum.Text = "label2";
+            // 
+            // bigChar
+            // 
+            this.big.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.big.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.big.Location = new System.Drawing.Point(80, 153);
+            this.big.Name = "bigChar";
+            this.big.Size = new System.Drawing.Size(510, 28);
+            this.big.TabIndex = 2;
+            this.big.Text = "label1";
+            // 
+            // special
+            // 
+            this.special.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.special.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.special.Location = new System.Drawing.Point(80, 203);
+            this.special.Name = "special";
+            this.special.Size = new System.Drawing.Size(510, 28);
+            this.special.TabIndex = 3;
+            this.special.Text = "label3";
+            // 
+            // num
+            // 
+            this.digit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.digit.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.digit.Location = new System.Drawing.Point(80, 251);
+            this.digit.Name = "num";
+            this.digit.Size = new System.Drawing.Size(510, 28);
+            this.digit.TabIndex = 4;
+            this.digit.Text = "label4";
+            // 
+            // imCharNum
+            // 
+            this.imChar.Image = global::Kontrolki.Properties.Resources._true;
+            this.imChar.Location = new System.Drawing.Point(14, 93);
+            this.imChar.Name = "imCharNum";
+            this.imChar.Size = new System.Drawing.Size(45, 42);
+            this.imChar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imChar.TabIndex = 5;
+            this.imChar.TabStop = false;
+            // 
+            // imBigCar
+            // 
+            this.imBig.Image = global::Kontrolki.Properties.Resources._true;
+            this.imBig.Location = new System.Drawing.Point(14, 141);
+            this.imBig.Name = "imBigCar";
+            this.imBig.Size = new System.Drawing.Size(45, 42);
+            this.imBig.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imBig.TabIndex = 6;
+            this.imBig.TabStop = false;
+            // 
+            // imSpecial
+            // 
+            this.imSpecial.Image = global::Kontrolki.Properties.Resources._true;
+            this.imSpecial.Location = new System.Drawing.Point(14, 189);
+            this.imSpecial.Name = "imSpecial";
+            this.imSpecial.Size = new System.Drawing.Size(45, 42);
+            this.imSpecial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imSpecial.TabIndex = 7;
+            this.imSpecial.TabStop = false;
+            // 
+            // imNum
+            // 
+            this.imDigit.Image = global::Kontrolki.Properties.Resources._true;
+            this.imDigit.Location = new System.Drawing.Point(14, 237);
+            this.imDigit.Name = "imNum";
+            this.imDigit.Size = new System.Drawing.Size(45, 42);
+            this.imDigit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imDigit.TabIndex = 8;
+            this.imDigit.TabStop = false;
+            // 
+            // PasswordCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.imDigit);
+            this.Controls.Add(this.imSpecial);
+            this.Controls.Add(this.imBig);
+            this.Controls.Add(this.imChar);
+            this.Controls.Add(this.digit);
+            this.Controls.Add(this.special);
+            this.Controls.Add(this.big);
+            this.Controls.Add(this.charNum);
+            this.Controls.Add(this.title);
+            this.Name = "PasswordCheck";
+            this.Size = new System.Drawing.Size(590, 306);
+            ((System.ComponentModel.ISupportInitialize)(this.imChar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imBig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imSpecial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imDigit)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label title;
+        private Label charNum;
+        private Label big;
+        private Label special;
+        private Label digit;
+        private PictureBox imChar;
+        private PictureBox imBig;
+        private PictureBox imSpecial;
+        private PictureBox imDigit;
+    }
+}
